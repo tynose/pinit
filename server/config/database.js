91cf373d1531ab0e969103966ce22779e1f3f0ba
tynose <tylernoseworthy21@gmail.com>
2019-02-16 17:08:30 -0500

    init

--- server/config/database.js
+++ server/config/database.js
@@ -1,19 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// module.exports = new Sequelize(process.env.DATABASE_URL, {
-// 	dialect: 'postgres',
-// 	protocol: 'postgres',
-// 	dialectOptions: {
-// 		ssl: true
-// 	},
-// 	operatorsAliases: Sequelize.Op
-// });
-
-module.exports = new Sequelize('tread', 'tylernoseworthy', 'root', {
-	host: 'localhost',
+module.exports = new Sequelize(process.env.DATABASE_URL, {
 	dialect: 'postgres',
-	port: 5432,
-	operatorsAliases: false,
-	logging: false
+	protocol: 'postgres',
+	dialectOptions: {
+		ssl: true
+	},
+	operatorsAliases: Sequelize.Op
 });
